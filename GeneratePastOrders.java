921ba9df08b3cabee2a92386f6257a7a3807c422
KyleDOwens <90162505+KyleDOwens@users.noreply.github.com>
2022-06-10 22:35:37 +0200

    Updated GeneratePastOrders.java
    GeneratePastOrders.java will now populate the entire CustomerOrder and CustomerSaleLine tables, and can be easily modified to populate the VendorOrder and VendorSaleLine tables as well

--- GeneratePastOrders.java
+++ GeneratePastOrders.java
@@ -2,22 +2,21 @@ import java.sql.*;
 import java.io.*;
 import java.util.*;
 
-
+/* Our Primary Key ID Layout: */
 // foodID: 1 to 100
 // EmployeeID: 101 to 200
-// saleLineID: 201 to 600
-// buyLineID: 601 to 1,000
-// VendorOrderID: 1,001 to 100,000
-// CustomerOrderID: 100,001 to 1,000,000
+
+// VendorOrderID: 1,001 to 10,000
+// CustomerOrderID: 10,001 to 20,000
+
+// buyLineID: 100,000 to 1,000,000
+// saleLineID: 1,000,001 to 2,000,000
 
 
-
-
-
-
-
+// This file will populate the CustomerOrder and CustomerSaleLine database tables.
+// If we change the database names within the file, we can also populate the vendorOrder and vendorBuyLine tables.
 public class GeneratePastOrders {
-    static String sqlQuery(String command, boolean needResult) {
+    static String sqlQuery(String command, String needResult) {
         Connection conn = null;
         String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315950_1db";
 
@@ -35,11 +34,11 @@ public class GeneratePastOrders {
             Statement stmt = conn.createStatement();
             String sqlStatement = command;
 
-            if (needResult) {
+            if (needResult.length() > 0) {
                 ResultSet result = stmt.executeQuery(sqlStatement);
                 
                 while (result.next()) {
-                    return result.getString("unitPrice");
+                    return result.getString(needResult);
                 }
             }
             else {
@@ -58,12 +57,11 @@ public class GeneratePastOrders {
     }
 
 
-    static double GenerateOrder(int customerID, String orderDate) {
+    static double GenerateOrder(int customerID, String orderDate, int saleLineID) {
         int randomFoodID;
         double randomMass;
         double saleLinePrice;
         double orderTotal = 0;
-        int saleLineID = 1000;
         int customerOrderId = customerID;
         
         // Generates a single random order        
@@ -74,25 +72,25 @@ public class GeneratePastOrders {
             // Generate an ID between 1 and 55
             // Generate a mass between 0.25 and 3.75
             randomFoodID = rand.nextInt(55) + 1;
-            randomMass = 3.5 * rand.nextDouble() + 0.25;
+            randomMass = 1.5 * rand.nextDouble() + 0.25;
             
             // Truncate randomMass to 2 decimal places
             randomMass = ( (double) ((int)(randomMass * 100) ) ) / 100;
 
             // Query for unit price given the foodID
-            double unitPrice = Double.parseDouble(sqlQuery("SELECT unitPrice FROM FoodItems WHERE foodID='" + randomFoodID + "';", true));
+            double unitPrice = Double.parseDouble(sqlQuery("SELECT unitPrice FROM FoodItems WHERE foodID='" + randomFoodID + "';", "unitPrice"));
             
             saleLinePrice = unitPrice * randomMass;
             orderTotal += saleLinePrice;
             // Add saleLine to database
             sqlQuery("INSERT INTO CustomerSaleLine(saleLineID, customerOrderID, foodID, saleLinePrice, saleLineQuantity) VALUES(" + 
-            saleLineID + ", " + customerOrderId + ", " + randomFoodID + ", " + saleLinePrice + ", " + randomMass + ");", false);
+            saleLineID + ", " + customerOrderId + ", " + randomFoodID + ", " + saleLinePrice + ", " + randomMass + ");", "");
         
             saleLineID++;
         }
 
         // Add the order to CustomerOrder database
-        sqlQuery("UPDATE CustomerOrder SET customerOrderTotal = " + orderTotal + "WHERE customerOrderID = '" + customerID + "';", false);
+        sqlQuery("UPDATE CustomerOrder SET customerOrderTotal = " + orderTotal + "WHERE customerOrderID = '" + customerID + "';", "");
 
         return orderTotal;
     }
@@ -100,19 +98,34 @@ public class GeneratePastOrders {
 
     public static void main(String args[]) {
         Random rand = new Random();
-        int customerID = 1;
-        String date = "2022-06-10";
-        int employeeID = rand.nextInt(4) + 1;
+        int customerID = 10001;
+        int dayOfTheMonth = 10;
+        String date = "2022-06-" + dayOfTheMonth;
         String[] allPaymentMethods = {"Cash", "Credit", "Debit"};
-        String paymentMethod = allPaymentMethods[rand.nextInt(3)];
         double dailyTotal = 0;
+        double salesGoalTotal = 0;
+        int startSaleLineID = 1000001;
 
-        // while(dailyTotal < 1100) {
+        while(salesGoalTotal < 18000) {
+            int employeeID = rand.nextInt(4) + 101;
+            String paymentMethod = allPaymentMethods[rand.nextInt(3)];
 
-        // }
-        sqlQuery("INSERT INTO CustomerOrder(customerOrderID, customerOrderDate, customerOrderTotal, paymentMethod, employeeID) VALUES(" +
-        customerID + ", '" + date + "', 0.01, '" + paymentMethod + "', " + employeeID + ");", false);
+            sqlQuery("INSERT INTO CustomerOrder(customerOrderID, customerOrderDate, customerOrderTotal, paymentMethod, employeeID) VALUES(" +
+            customerID + ", '" + date + "', 0.01, '" + paymentMethod + "', " + employeeID + ");", "");
 
-        GenerateOrder(customerID, date);
+            double tempOrderTotal = GenerateOrder(customerID, date, startSaleLineID);
+            dailyTotal += tempOrderTotal;
+            salesGoalTotal += tempOrderTotal;
+
+            if(dailyTotal > 1000) {
+                dayOfTheMonth += 1;
+                dailyTotal = 0;
+                date = "2022-06-" + dayOfTheMonth;
+            }
+
+            customerID++;
+
+            startSaleLineID = 1 + Integer.valueOf(sqlQuery("SELECT * FROM CustomerSaleLine WHERE saleLineID=(SELECT max(saleLineID) FROM CustomerSaleLine);", "saleLineID"));
+        }
     }
 }
