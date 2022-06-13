60d2beac6b2d834005ca8d1c03f1f06c31706d8f
mabarry <mbarry4602@tamu.edu>
2022-06-13 16:56:27 +0200

    Created initial template for the GUI project files.

---
+++ POS-System-GUI/src/main/java/com/example/possystemgui/databaseGUI.java
@@ -0,0 +1,16 @@
+package com.example.possystemgui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class databaseGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
