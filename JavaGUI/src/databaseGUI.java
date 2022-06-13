83fb3970e478825313b36d2002bb852b6512a29b
mabarry <mbarry4602@tamu.edu>
2022-06-14 00:08:19 +0200

    Working button which changes the text field in the window.

--- JavaGUI/src/databaseGUI.java
+++ JavaGUI/src/databaseGUI.java
@@ -1,7 +1,33 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // The main page for the POS System
 public class databaseGUI {
 
+     public static void main(String[] args) {
+         // Initialize a new Swing window to appear when run
+         JFrame frame = new JFrame();
 
+         // Text box that will appear when button is clicked
+         final JTextField textField = new JTextField();
+         textField.setBounds(50,50,150,20);
+
+         // Initializing button
+         JButton button = new JButton("Click");
+         button.setBounds(130,100,100,40);
+         button.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 textField.setText("The button was clicked!");
+             }
+         });
+
+         frame.add(button);
+         frame.add(textField);
+
+         frame.setSize(1280,720);
+         frame.setLayout(null);
+         frame.setVisible(true);
+     }
 }
