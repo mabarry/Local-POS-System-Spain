1580c20cd0b3eacf7befaa7f5c9948d976e6f883
mabarry <mbarry4602@tamu.edu>
2022-06-15 10:25:52 +0200

    Added function header comments

--- JavaGUI/src/databaseGUI.java
+++ JavaGUI/src/databaseGUI.java
@@ -155,8 +155,8 @@ public class databaseGUI {
         frame.add(cancelRow);
     }
 
+    /* Default sale view of items searched and returned from the database */
     public static JTextField ScreenSpace(JFrame frame) {
-
         // Output text field, determined by TextField input
         final JTextField output = CreateTextField(545,90,945,300, "");
 
@@ -167,6 +167,7 @@ public class databaseGUI {
         return output;
     }
 
+    /* Creates the sale table to be used during checkout */
     public static JTable CreateTable(JFrame frame) {
         JTable table = new JTable(tableModel);
         table.setBounds(100,40,200,200);
@@ -182,33 +183,36 @@ public class databaseGUI {
         return table;
     }
 
+    /* Streamlines insertion process of data into DefaultTableModels to one line of code */
     public static void InsertIntoTable(JFrame frame, String[] row, DefaultTableModel DFT) {
         // TODO: Populate JTable
         DFT.insertRow(DFT.getRowCount(), row);
     }
 
     public static void ScreenSwitcher(JFrame frame, JTable inventory, JTable employees) {
-        // creates button to change views
+        // Create different views and the buttons to switch those views
         JButton managerView = CreateButton(545,550, 465, 105, "Switch to Manager View");
         JButton employeeView = CreateButton(1022, 550, 465, 105, "Switch to Employee View (Sales)");
         JButton updateItems = CreateButton(545, 660, 465, 105, "Update Inventory Item(s)");
-        JPanel cover = new JPanel();
+        JPanel coverInventory = new JPanel();
         JPanel coverEmployee = new JPanel();
 
-        //Create a border
+        // Black border to be used by both screen banels
         Border blackLine = BorderFactory.createLineBorder(Color.black);
 
-        // Adjusts backPanel properties
-        cover.setBounds(545,90,472,450);
-        cover.setBorder(blackLine);
-        cover.setBackground(Color.white);
-        cover.add(new JScrollPane(inventory));
+        // GUI field to store inventory information
+        coverInventory.setBounds(545,90,472,450);
+        coverInventory.setBorder(blackLine);
+        coverInventory.setBackground(Color.white);
+        coverInventory.add(new JScrollPane(inventory));
 
+        // GUI field to store employee information
         coverEmployee.setBounds(1022,90,472,450);
         coverEmployee.setBorder(blackLine);
         coverEmployee.setBackground(Color.white);
         coverEmployee.add(new JScrollPane(employees));
 
+        /* When manager view is pressed, new tables will appear that the manager is able to edit and view */
         managerView.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -216,13 +220,15 @@ public class databaseGUI {
                 frame.remove(output);
                 frame.remove(quantity);
                 frame.add(updateItems);
-                frame.add(cover);
+                frame.add(coverInventory);
                 frame.add(coverEmployee);
                 frame.repaint();
                 frame.revalidate();
             }
         });
 
+        /* When employee view is pressed, all the managerial aspects and views of the GUI are hidden and rendered
+        *  usable, effectively switching the user's permissions */
         employeeView.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -230,12 +236,14 @@ public class databaseGUI {
                 frame.add(output);
                 frame.add(quantity);
                 frame.remove(updateItems);
-                frame.remove(cover);
+                frame.remove(coverInventory);
                 frame.remove(coverEmployee);
                 frame.repaint();
             }
         });
 
+        /* When updateItems is pressed, the inventory in our database will be updated to hold the new values
+        *  input by the manager */
         updateItems.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -247,9 +255,9 @@ public class databaseGUI {
         // adds elements to frame
         frame.add(managerView);
         frame.add(employeeView);
-
     }
 
+    /* Create table to store food and item data from inventory that managers can view and edit */
     public static JTable InventoryTable(JFrame frame) {
         JTable inventory = new JTable(inventoryTable);
         inventory.setBounds(10,10,200,200);
@@ -267,6 +275,7 @@ public class databaseGUI {
         return inventory;
     }
 
+    /* Create table to store employee data that managers can view */
     public static JTable EmployeeTable(JFrame frame) {
         JTable employees = new JTable(employeeTable);
         employees.setBounds(10,10,200,200);
@@ -283,18 +292,25 @@ public class databaseGUI {
     public static void main(String[] args) {
         // Initialize a new Swing window to appear when run
         JFrame frame = new JFrame("Fruit Stand Point of Sales");
+
+        // Tables being initialized
         JTable table = CreateTable(frame);
         JTable inventory = InventoryTable(frame);
         JTable employees = EmployeeTable(frame);
+
+        // Global buttons and boxes initialized
         moveToSale = CreateButton(1022, 400, 465, 105, "Move to Sale");
         output = ScreenSpace(frame);
         quantity = CreateTextField(545,400,462,105, "Enter Qty. of Item");
+
+        // Add global buttons and boxes to sale to be used in all functions
         frame.add(moveToSale);
         frame.add(quantity);
         frame.add(output);
 
         // TODO: call create customer order
 
+        // Initialize the search bar, sales box, and system to switch between management and employee views
         SearchBar(frame);
         ReceiptBox(frame, table);
         ScreenSwitcher(frame, inventory, employees);
